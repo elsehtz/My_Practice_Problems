71392b349eb60d01dfef5be2cfa12f943dc07a0d
Zak <elseht17@gmail.com>
2019-07-17 23:11:59 -0400

    added script thats has write() function for reference

---
+++ write_to_file.py
@@ -0,0 +1,34 @@
+import sys
+import datetime
+import numpy as np
+import os
+
+# file1 = open("<file_name.type>","w") 'w' writes over file
+# file1 = open("<file_name.type>","a") 'a' appends and write() data onto the current contents
+
+def main():
+    # Case 1: txt
+    file_type = input("What kind of file are you writing to? (txt, json, csv)\n").lower()
+    if(file_type == 'txt'):
+        file1 = open("write_to_file_practice.txt","w")# w for write mode, a for append mode
+        file1.write(input("Type something to put into example txt file\n"))
+        print("Check write_to_file_practice.txt in the directory to see!")
+    # Case 2: csv
+    elif(file_type == 'csv'):
+        file1 = open("write_to_file_practice.csv","w")# w for write mode, a for append mode
+        file1.write('it worked')
+        print("String placed in example csv file, try real data next!")
+
+    # Case 3: JSON
+    elif(file_type == 'json'):
+        file1 = open("write_to_file_practice.json","w")# w for write mode, a for append mode
+        file1.write("Placed holder for example write to json")
+    
+    else:
+        print("Sorry, file specificed not recognized. Approved types are txt, json, and csv")
+    
+    return
+
+if __name__ == '__main__':
+    
+    main()
